2a98c9327be7bca770bcd2045d4e07e82da535f2
MIV3L <niemitooceniac@gmail.com>
2023-11-30 14:30:05 +0100

    kom zadanko

--- Struktury Programów/zadan.cpp
+++ Struktury Programów/zadan.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 
 {
-	for (int i = 15; i <= 50; ++i) {
-		if (i % 5 == 0) {
-			cout << i<< " ";
+	for (int i = 15; i <= 50; ++i) { // petla for bedzie dzialac dopoki zmienna i bedzie mniejsza lub rowna 50 
+		if (i % 5 == 0) { // sprawdza czy reszta z dzielenia przez 5 wynosi 0 
+			cout << i<< " "; // jesli spelnia warunek to dzielona liczba jest wyswietlana na ekanie
 			
 		}
 	}
